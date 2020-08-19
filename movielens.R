a401e45b57b2de0c7078e5c937342341f61c83fc
AmandaRP <amanda@dekleineorchards.com>
2020-08-19 22:36:05 +0000

    Add code to read movielens data

---
+++ movielens.R
@@ -0,0 +1,103 @@
+# This script reads and wrangles the movielens data
+
+# Read data ---------------------------------------------------------------
+
+# notes: 
+# - Movie Lense (ml-1m) data
+# - user & item indexes are 0 based.
+# - train.rating contains user/item/rating triplets (plus a timestamp)
+# - test.negative contains a list of 100 negatives and one positive for each user
+# - test.rating contains user/item/rating triplets (plus a timestamp) for 
+#   each user's single rating in test.negative. 
+
+base_name <- "https://github.com/hexiangnan/neural_collaborative_filtering/raw/master/Data/ml-1m"
+
+url <- str_c(base_name, ".train.rating")
+train_rating <- read_tsv(pins::pin(url), 
+                         col_names = c("user","item", "rating", "timestamp")) 
+
+url <-  str_c(base_name, ".test.negative")
+test_data <- pins::pin(url) 
+#test_rating   <- read_tsv(test_data[str_detect(test_data, "rating")], 
+#                          col_names = c("user","item", "rating", "timestamp"))
+test_negative <- read_tsv(test_data[str_detect(test_data, "negative")], 
+                          col_names = FALSE) 
+
+#See pins issue: https://github.com/rstudio/pins/issues/271
+#url <- "https://github.com/hexiangnan/neural_collaborative_filtering/raw/master/Data/ml-1m.test.negative"
+#test_negative <- read_tsv(pins::pin(url), 
+#                          col_names = FALSE) 
+
+
+# Variable definitions ----------------------------------------------------
+
+num_users <- max(train_rating$user) + 1
+num_items <- max(train_rating$item) + 1
+neg_pos_ratio_train <- 5 # Ratio of negative training samples to positive to select for training. NCF authors used 4.
+
+
+# Data wrangling ----------------------------------------------------------
+
+# Test set
+test <- test_negative %>% 
+  tidyr::extract(X1, into = c("user", "pos_item"), "([[:alnum:]]+),([[:alnum:]]+)", convert = TRUE) %>%
+  pivot_longer(cols = pos_item:X100, names_to = "label", values_to = "item") %>%
+  mutate(label = as.integer(!str_detect(label,"X")))
+
+# Get number of training ratings per user (will need for sampling negatives)
+num_ratings_per_user <- train_rating %>% group_by(user) %>% count()
+
+# Define negatives (those movies that were not rated for each user) to use for training 
+train_negative <- 
+  data.frame(user = rep(0:(num_users-1), each=num_items), 
+             item = 0:(num_items-1)) %>% # start by listing all user/item pairs 
+  anti_join(train_rating) %>% # remove user/item pairs that are in positive training set 
+  anti_join(test) %>%       # remove user/item pairs that are in test set
+  group_by(user) %>%  # Remaining operations used for sampling some of the negatives based on chosen negative to positive ratio
+  nest() %>%
+  inner_join(num_ratings_per_user) %>%
+  mutate(subsamp = map2(data, n*neg_pos_ratio_train, ~slice_sample(.x, n=.y))) %>% 
+  select(user, subsamp) %>%
+  unnest(cols = c(subsamp))
+
+# Define validation data by picking the most recent rating for each user from training
+validation <- train_rating %>% 
+  group_by(user) %>% 
+  slice_max(timestamp) %>% 
+  slice_sample(1) %>% #some user/item pairs have same timestamp, so randomly pick one
+  select(user, item) %>%
+  add_column(label = 1)  # Only positive class was sampled for validation. See section 4.1 of NCF paper.
+
+# Define training as data not used for validation
+train <- anti_join(train_rating, validation) %>% 
+  select(user, item) %>%
+  bind_rows("pos" = ., "neg" = train_negative, .id = "label") %>%
+  mutate(label = as.integer(str_detect(label,"pos")))
+
+
+
+# Movielens 10M
+
+tmp <- tempfile()
+download.file("http://files.grouplens.org/datasets/movielens/ml-latest-small.zip", tmp)
+test <- jsonlite::stream_in(unz(tmp, "/ml-latest-small/ratings.csv)"))
+
+library(tidyverse)
+library(magrittr)
+ratings <- read_csv("ml-latest-small/ratings.csv") %>% filter(rating >=4 )
+
+# Iteratively remove users and items that have fewer than 5 ratings.
+threshold <- 5
+stop <- FALSE
+while(!stop){
+  num_ratings_before <- nrow(ratings)
+  ratings %<>% inner_join(ratings %>% group_by(userId) %>% count() %>% filter(n >= threshold) %>% select(userId))
+  ratings %<>% inner_join(ratings %>% group_by(movieId) %>% count() %>% filter(n >= threshold) %>% select(movieId))
+  stop <- if_else(num_ratings_before == nrow(ratings), TRUE, FALSE)
+}
+  
+#Unique list of users and items
+userIDs <- ratings %>% select(userId) %>% distinct()
+movieIDs <- ratings %>% select(movieId) %>% distinct()
+
+#Next: For each user, hold out 20% of ratings for test.
